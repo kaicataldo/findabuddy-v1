b498b76402c5479ce87276573f0caacb3a7eb570
Kai Cataldo <kaicataldo@gmail.com>
2014-10-04 02:25:14 -0400

    Image picker added
    Still won’t post images to Twitter

--- findabuddybot.js
+++ findabuddybot.js
@@ -13,50 +13,70 @@ var name;
 var sex;
 var breed;
 var mix;
+var picArray;
 var picture;
 var id;
 var startPhrase;
 var endPhrase;
 var buddyTweet;
 
-randomizer();
-var url = 'http://api.petfinder.com/pet.find?key=a6c914ff39bbb7d1cc3c0ead2efa3494&animal=dog&location=new%20york%20ny&count=1&offset=' + offset + '&output=full&format=json';
+var getRequest = function() {
+  randomizer();
+  var url = 'http://api.petfinder.com/pet.find?key=a6c914ff39bbb7d1cc3c0ead2efa3494&animal=dog&location=new%20york%20ny&count=1&offset=' + offset + '&output=full&format=json';
 
-request(url, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    parsedData = JSON.parse(body);
-    dogData = parsedData.petfinder.pets.pet;
-    console.log(dogData);
-    name = dogData.name.$t;
-    sex = sex(dogData.sex.$t);
-    breed = dogData.breeds.breed.$t ? dogData.breeds.breed.$t : 'doggie';
-    mix = mix(dogData.mix.$t);
-    id = link(dogData.id.$t);
-    startPhrase = name + " is a " + sex + " " + breed + mix;
-    endPhrase = endOfSentence();
-    buddyTweet = startPhrase + endPhrase + " - " + id;
-    postTweet();
-  }
-  else {
-    console.log('Error!');
-  }
-});
-
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      parsedData = JSON.parse(body);
+      dogData = parsedData.petfinder.pets.pet;
+      picArray = dogData.media.photos.photo;
+      
+      name = dogData.name.$t;
+      sex = sex(dogData.sex.$t);
+      breed = dogData.breeds.breed.$t ? dogData.breeds.breed.$t : 'doggie';
+      mix = mix(dogData.mix.$t);
+      id = link(dogData.id.$t);
+      startPhrase = name + " is a " + sex + " " + breed + mix;
+      endPhrase = endOfSentence();
+      buddyTweet = startPhrase + endPhrase + " - " + id;
+      pickPic(picArray);
+      console.log(picture);
+      postTweet();
+    }
+    else {
+      console.log('Error!');
+    }
+  });
+};
 
 function postTweet() {
-  twitter.statuses("update", {
-          status: buddyTweet
+  twitter.statuses("update_with_media", {
+        media: [
+          picture,
+        ],
+        status: buddyTweet
       },
       '2789586862-sIVGU2GNXyRlwBOByLKTvgHZXeLA8SqbHGC2mbt',
       '8essmESqBGAZyrLmA502uIR9JuRLcgktLrbsbECI5Pq2f',
       function(error, data, response) {
           if (error) {
-              // something went wrong
+              console.log(response);
           } else {
-              // data contains the data sent by twitter
+           console.log("Success!");
           }
       }
   );
+}
+
+function pickPic(photos) {
+  photo = 0;
+  while ( photos[photo]['@size'] !== 'x' ) {
+      photo++;
+      if ( photo === photos.length ) { 
+        getRequest();
+      }
+      // set the URL equal to the (new?) photo's URL
+      picture = "\"" + photos[photo].$t + "\"";
+  }
 }
 
 /*setInterval(function() {
@@ -70,7 +90,7 @@ function link(idNumber) {
 }
 
 function endOfSentence() {
-  var phrases = [' who needs a loving home!', ' looking for a new family!', ' looking for a furever home!', ' who needs a new best friend!', ' who needs a place to call home!', ' looking for a forever home!', ' who wants to be your buddy!', ' looking for a living family!', ' in need of love!', ' in need of a loving home!', ' looking for a new home!', ' who needs some lovin\'!', ' who could be your new buddy!'];
+  var phrases = [' who needs a loving home!', ' looking for a new family!', ' looking for a furever home!', ' who needs a new best friend!', ' who needs a place to call home!', ' looking for a forever home!', ' who wants to be your buddy!', ' looking for a loving family!', ' in need of love!', ' in need of a loving home!', ' looking for a new home!', ' who needs some lovin\'!', ' who could be your new buddy!'];
   endPhrase = phrases[Math.floor(Math.random() * phrases.length)];
   return endPhrase;
 }
@@ -97,3 +117,5 @@ function randomizer() {
   offset = Math.round(Math.random() * 1999);
   console.log(offset);
 }
+
+getRequest();
