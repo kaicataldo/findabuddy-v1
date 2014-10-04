4634291ffcbed5528c3b2d9073f7d5971ca24886
Kai Cataldo <kaicataldo@gmail.com>
2014-10-03 23:26:52 -0400

    Revert "REDO REDO rebuilt with twitter-api"
    This reverts commit 1a81b96e92e7ce652e7d7b931c000cc82b33ec9f.

--- findabuddybot.js
+++ findabuddybot.js
@@ -2,8 +2,8 @@ var twitterAPI = require('node-twitter-api'),
     request = require('request');
 
 var twitter = new twitterAPI({
-    consumerKey: CONSUMERKEY,
-    consumerSecret: CONSUMERSECRET,
+    consumerKey: 'cXepHhzKviY0EN4lUYS9gVp2Z',
+    consumerSecret: 'Hm21dp9xQrCo7JfJ1kHvQT8v1yFQO2eZpbxU0geTjbQzAhWpOD',
     callback: 'http://localhost:8080'
 });
 
@@ -45,17 +45,13 @@ request(url, function (error, response, body) {
 
 function postTweet() {
   twitter.statuses("update", {
-        media: [
-          picture,
-          stream
-    ],
           status: buddyTweet
       },
-      TOKEN,
-      TOKENSECRET,
+      '2789586862-sIVGU2GNXyRlwBOByLKTvgHZXeLA8SqbHGC2mbt',
+      '8essmESqBGAZyrLmA502uIR9JuRLcgktLrbsbECI5Pq2f',
       function(error, data, response) {
           if (error) {
-              console.log('Error!');
+              // something went wrong
           } else {
               // data contains the data sent by twitter
           }
@@ -68,10 +64,6 @@ function postTweet() {
     if(err) return handleError(err);
   });
 }, 3600000);*/
-
-function pickPic() {
-
-}
 
 function link(idNumber) {
  return "https://www.petfinder.com/petdetail/" + idNumber + "/";
